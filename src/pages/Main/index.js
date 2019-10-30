fe86138bf5d31a1e2ab7404b57faf2c4d3edb68d
elitonlunardi <lunardi.eliton@gmail.com>
2019-10-29 21:10:50 -0300

    finalizado a parte de detalhes do repositorio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton, List } from './styles';
 
+import Container from '../../components/Container/index';
+import { Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
 
 export default class Main extends Component {
@@ -83,7 +85,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="a">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
